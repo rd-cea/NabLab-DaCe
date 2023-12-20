28d2e308d8d17eddb3e46845e0419521960b7fa6
Rémi Dehenne <remi.dehenne@cea.fr>
2023-12-20 15:26:48 +0100

    [doc] Update copyright headers
    2022 -> 2023
    
    Signed-off-by: Rémi Dehenne <remi.dehenne@cea.fr>

--- plugins/fr.cea.nabla.cpplib/src/nablalib/nablalib/utils/kokkos/Serializer.h
+++ plugins/fr.cea.nabla.cpplib/src/nablalib/nablalib/utils/kokkos/Serializer.h
@@ -1,5 +1,5 @@
 /*******************************************************************************
-* Copyright (c) 2022 CEA
+* Copyright (c) 2023 CEA
 >>>>>>> [dev] Store leveldb values as binary
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
